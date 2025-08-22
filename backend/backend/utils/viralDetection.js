b3ccf6fb7c194991ed3725f9d1f03ec23cba4c5f
tidjane7 <ahmedtidianie700@gmail.com>
2025-08-22 23:47:56 +0000

    Create viralDetection.js

---
+++ backend/backend/utils/viralDetection.js
@@ -0,0 +1,9 @@
+function detectViralMoments(videoData) {
+  return [
+    { start: 5, end: 15 },
+    { start: 30, end: 45 },
+    { start: 60, end: 75 }
+  ];
+}
+
+module.exports = { detectViralMoments };
